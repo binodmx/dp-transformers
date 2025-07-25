a2bebc982a9aad5d4a533f0e300d487613ff2069
binod <binodmx@gmail.com>
2025-07-25 16:27:16 +1000

    Fix #1

--- src/dp_transformers/dp_utils.py
+++ src/dp_transformers/dp_utils.py
@@ -236,7 +236,7 @@ class OpacusDPTrainer(Trainer):
 
         return self.optimizer
 
-    def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]]) -> torch.Tensor:
+    def training_step(self, model: nn.Module, inputs: Dict[str, Union[torch.Tensor, Any]], **kwargs) -> torch.Tensor:
         """
         Perform a training step on a batch of inputs.
 
